1ff94ec66d4a4539331c49b2dc56fe027136e08b
KaptainCS3 <mbiapplegryn@gmail.com>
2022-10-03 15:29:50 +0100

    add indicator hover effect

--- ecommerce-product-page-main/app.js
+++ ecommerce-product-page-main/app.js
@@ -1,47 +1,60 @@
 const tabs = document.querySelectorAll("[data-tab-target]");
 const tabContents = document.querySelectorAll("[data-tab-content]");
-const thumbnail = document.querySelectorAll("[data-tab-thumbnail]")
-const btnMinus = document.querySelector('.decrement')
-let quantityValue = document.querySelector('.quantity-value');
+const thumbnail = document.querySelectorAll("[data-tab-thumbnail]");
+const btnMinus = document.querySelector(".decrement");
+let quantityValue = document.querySelector(".quantity-value");
 tabs.forEach((tab) => {
-    tab.addEventListener('click', ()=>{
-        const target = document.querySelector(tab.dataset.tabTarget);
-        // loop through all tab content
-        tabContents.forEach(tabContent =>{
-            tabContent.classList.remove('active')
-        })
-         tabs.forEach((tab) => {
-           tab.classList.remove("active");
-         });         
-        tab.classList.add("active")
-        target.classList.add("active")
-    })
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    // loop through all tab content
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+    target.classList.add("active");
+  });
 });
 
-
 // quantity add / reduce function
-const increaseValue = document.querySelector('.increment')
-const decreaseValue = document.querySelector('.decrement')
+const increaseValue = document.querySelector(".increment");
+const decreaseValue = document.querySelector(".decrement");
 let qty = 0;
-let qtyValue = document.querySelector('.quantity-value')
-increaseValue.addEventListener('click', ()=>{
-  qty += 1
-  qtyValue.innerHTML = qty
-})
-decreaseValue.addEventListener('click', ()=>{
-  if(qty <= 1)
-  return
-  qty -= 1
-  qtyValue.innerHTML = qty
-})
+let qtyValue = document.querySelector(".quantity-value");
+increaseValue.addEventListener("click", () => {
+  qty += 1;
+  qtyValue.innerHTML = qty;
+});
+decreaseValue.addEventListener("click", () => {
+  if (qty <= 1) return;
+  qty -= 1;
+  qtyValue.innerHTML = qty;
+});
 
-const lightBox = document.createElement('div');
-lightBox.id = 'lightbox';
+const lightBox = document.createElement("div");
+lightBox.id = "lightbox";
 document.body.appendChild(lightBox);
 
-const preview = document.querySelectorAll('.preview-content');
-preview.forEach(tab =>{
-  tab.addEventListener('click', () =>{
-    lightBox.classList.add('active')
-  })
-})
+const preview = document.querySelectorAll(".preview-content");
+preview.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    lightBox.classList.add("active");
+  });
+});
+
+// nav bar
+const markerNav = document.querySelector("#marker");
+const slideNav = document.querySelectorAll("[data-slide-nav]");
+
+// active state navbar
+function indicatorNav(f) {
+  markerNav.style.left = f.offsetLeft + "px";
+  markerNav.style.width = f.offsetWidth + "px";
+}
+slideNav.forEach((link) => {
+  link.addEventListener("mouseover", (f) => {
+    indicatorNav(f.target);
+  });
+});
