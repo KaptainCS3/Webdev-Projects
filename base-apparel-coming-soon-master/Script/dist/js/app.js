5053d3d00be1412391f2656b094874495dde01c0
KaptainCS3 <mbiapplegryn@gmail.com>
2022-11-02 01:10:57 +0100

    mobile responsive in progress

--- base-apparel-coming-soon-master/Script/dist/js/app.js
+++ base-apparel-coming-soon-master/Script/dist/js/app.js
@@ -1,34 +1,21 @@
-//form validation js
-//! Targetting form el 
-
-const form = document.getElementById("form_submit");
-const email = document.getElementById("email");
-
-form.addEventListener("submit", (e) =>{
-    e.preventDefault();
-    checkInput();
+const btn = document.querySelector("button");
+const errorMessage = document.querySelector(".error__email");
+const email = document.querySelector("#email");
+const error = document.querySelector(".error_control");
+const submit = document.querySelector("#form_submit");
+function handleSubmit(event) {
+  event.preventDefault();
+}
+submit.addEventListener("submit", handleSubmit)
+btn.addEventListener("click", () => {
+  if (!email.value.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+    email.style.borderColor = "#f96262";
+    error.style.display = "block";
+    errorMessage.innerHTML = "Please enter a valide email";
+    return false;
+  } else {
+    handleSubmit()
+    errorMessage.innerHTML = "";
+    return true;
+  }
 });
-
-function checkInput(){
-    const emailValue = email.value.trim();
-
-    if(emailValue === ''){
-        setErrorFor(email, 'email can not be blank');
-    }
-    else if(){
-
-    }
-}
-
-function setErrorFor(input, message){
-    const formControl = input.parentElement;
-    const small = document.querySelector('small');
-
-    small.innerText = message;
-    formControl = 'error_control error'
-}
-
-function isValidEmail(email){
-    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-}
-
