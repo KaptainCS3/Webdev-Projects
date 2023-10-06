b8d5fd6ab6b6f4b1433775a3b4e196b6b20c27af
KaptainCS3 <mbiapplegryn@gmail.com>
2023-10-06 23:11:18 +0100

    update tailwind config tsx and utility classes

--- digital-ads-software/tailwind.config.js
+++ digital-ads-software/tailwind.config.js
@@ -1,9 +1,42 @@
+/* eslint-env node */
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#008060",
+        },
+        secondary: {
+          DEFAULT: "#004c3f",
+        },
+        accent: {
+          DEFAULT: "#ffa881",
+        },
+        body: {
+          DEFAULT: "#677471",
+        },
+        dark_c: {
+          DEFAULT: "#001e19",
+        },
+        light_c: {
+          DEFAULT: "#fbf7ed",
+        },
+      },
+      fontFamily: {
+        textFont: ["sans-serif", "DM Sans"],
+        priSecAcc: ["Spline Sans", "san-serif"],
+      },
+    },
+    screens: {
+      sm: { min: "200px", max: "768px" },
+      // => @media (min-width: 200px and max-width: 768px) { ... }
+      md: { min: "769px", max: "990px" },
+      // => @media (min-width: 769px and max-width: 990px) { ... }
+      lg: { min: "991px" },
+      // => @media (min-width: 991px ) { ... }
+    },
   },
   plugins: [],
-}
-
+};
